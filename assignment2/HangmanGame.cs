a901e22aa4eb031425335e57458984c1a07ab3cd
Hugo Jiménez Barrasa <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 19:27:05 +0100

    Add files via upload

---
+++ assignment2/HangmanGame.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment2
+{
+    internal class HangmanGame
+    {
+        public string secretWord;
+        public string guessedWord;
+        public void Init(string secretWord)
+        {
+            this.secretWord = secretWord;
+            foreach (char c in secretWord)
+            {
+                guessedWord = guessedWord + ".";
+            }
+        }
+        public bool ContainsLetter(char letter)
+        {
+            foreach (char c in secretWord)
+            {
+                if (c == letter)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void ProcessLetter(char letter)
+        {
+            string dummyGuessedWord = null;
+            for (int i = 0; i < secretWord.Length; i++)
+            {
+                if (letter == secretWord[i])
+                {
+                    dummyGuessedWord += secretWord[i];
+                }
+                else
+                {
+                    dummyGuessedWord += guessedWord[i];
+                }
+            }
+            guessedWord = dummyGuessedWord;
+        }
+        public bool IsGuessed()
+        {
+            if (secretWord == guessedWord)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ assignment2/Program.cs
@@ -0,0 +1,112 @@
+﻿namespace assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("invalid number of arguments!");
+                Console.WriteLine("usage: assignment[2-3] <filename>");
+                return;
+            }
+            string filename = args[0];
+            Program myProgram = new Program();
+            myProgram.Start(filename);
+        }
+        void Start(string filename)
+        {
+            HangmanGame hangman = new HangmanGame();
+            List<string> words = new List<string>();
+            words = ListOfWords(filename);
+            hangman.Init(SelectWord(words));
+            DisplayWord(hangman.guessedWord);
+            bool victory = PlayHangman(hangman);
+            if (victory)
+            {
+                Console.WriteLine("You guessed the word!");
+            }
+            else
+            {
+                Console.WriteLine($"Too bad, you did not guess the word ({hangman.secretWord})");
+            }
+        }
+        List<string> ListOfWords(string filename)
+        {
+            List<string> secretWords = new List<string>();
+            StreamReader reader = new StreamReader(filename);
+            while (!reader.EndOfStream)
+            {
+                secretWords.Add(reader.ReadLine());
+            }
+            reader.Close();
+            return secretWords;
+        }
+        string SelectWord(List<string> words)
+        {
+            Random random = new Random();
+            string word;
+            do
+            {
+                word = words[random.Next(0, words.Count)];
+            }
+            while (word.Length < 3);
+            return word;
+        }
+
+        void DisplayWord(string word)
+        {
+            foreach (char c in word)
+            {
+                Console.Write($"{c} ");
+            }
+            Console.WriteLine();
+        }
+        char ReadLetter(List<char> blacklistLetters)
+        {
+            char letter;
+            do
+            {
+                Console.Write("Enter a letter: ");
+                letter = Convert.ToChar(Console.ReadLine());
+            }
+            while (blacklistLetters.Contains(letter));
+            return letter;
+        }
+        void DisplayLetters(List<char> letters)
+        {
+            Console.Write("Entered letters: ");
+            foreach (char c in letters)
+            {
+                Console.Write($"{c} ");
+            }
+            Console.WriteLine();
+        }
+        bool PlayHangman(HangmanGame hangman)
+        {
+            List<char> enteredLetters = new List<char>();
+            int attempts = 8;
+
+            while (attempts != 0)
+            {
+                char letter = ReadLetter(enteredLetters);
+                enteredLetters.Add(letter);
+                DisplayLetters(enteredLetters);
+                if (hangman.ContainsLetter(letter))
+                {
+                    hangman.ProcessLetter(letter);
+                }
+                else
+                    attempts--;
+                Console.WriteLine($"Attempts left: {attempts}");
+                Console.WriteLine();
+                DisplayWord(hangman.guessedWord);
+                if (hangman.IsGuessed())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
